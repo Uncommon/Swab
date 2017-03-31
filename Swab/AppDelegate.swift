049a104935e92c302ac6c1cbfa14f57b4eeb23e4
David Catmull <dtcatmull@xactware.com>
2017-03-31 14:26:53 -0600

    Reformatted

--- Swab/AppDelegate.swift
+++ Swab/AppDelegate.swift
@@ -1,26 +1,16 @@
-//
-//  AppDelegate.swift
-//  Swab
-//
-//  Created by David Catmull on 3/31/17.
-//  Copyright © 2017 David Catmull. All rights reserved.
-//
-
 import Cocoa
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
-  func applicationDidFinishLaunching(_ aNotification: Notification) {
+class AppDelegate: NSObject, NSApplicationDelegate
+{
+  func applicationDidFinishLaunching(_ aNotification: Notification)
+  {
     // Insert code here to initialize your application
   }
 
-  func applicationWillTerminate(_ aNotification: Notification) {
+  func applicationWillTerminate(_ aNotification: Notification)
+  {
     // Insert code here to tear down your application
   }
-
-
 }
 
