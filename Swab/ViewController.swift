5cd2887eb222225c11c493fb3b038074da39b326
David Catmull <dtcatmull@xactware.com>
2017-03-31 14:25:22 -0600

    Initial Commit

---
+++ Swab/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Swab
+//
+//  Created by David Catmull on 3/31/17.
+//  Copyright © 2017 David Catmull. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
